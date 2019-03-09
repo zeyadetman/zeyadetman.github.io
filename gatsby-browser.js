a43f28fcd7f45cdda5be7abbe265e888dfa3ab38
zeyadetman <zeyadetman@gmail.com>
2019-03-09 06:44:02 +0200

    code highlight

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,6 +6,4 @@
 
 // You can delete this file if you're not using it
 import wrapWithProvider from "./wrap-with-provider";
-require("prismjs/themes/prism-solarizedlight.css");
-require("prismjs/plugins/line-numbers/prism-line-numbers.css");
 export const wrapRootElement = wrapWithProvider;
--- src/components/layout.js
+++ src/components/layout.js
@@ -6,6 +6,9 @@ import { dayStyles, nightStyles } from "../styles/modeStyles";
 import Header from "./header";
 import { connect } from "react-redux";
 import "../styles/global-styles.css";
+import "prismjs/plugins/line-numbers/prism-line-numbers.css";
+import "prismjs/themes/prism-coy.css";
+import "../styles/prism-override.scss";
 
 const Layout = ({ children, mode, toggleMode }) => {
   const currentModeStyle = mode === "day" ? dayStyles : nightStyles;
