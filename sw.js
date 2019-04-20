30f1d914edc10bd2ccce961d907b3cb360c04413
zeyadetman <zeyadetman@gmail.com>
2019-04-20 05:21:59 +0200

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-475fc428515398a4d5ee.js"
+    "url": "webpack-runtime-a1232941155f7fbf3636.js"
   },
   {
     "url": "app-48ca27dedd81fef8a6e1.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "f49e2728c423ab120870708fb4c428b5"
+    "revision": "100129e5919dc7ceb0026366079f2962"
   },
   {
     "url": "styles.126006a9cf360d8d819e.css"
