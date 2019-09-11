d6407a8d7f9c03066e644822da8a25e213374b34
Zeyad Etman <zeyadetman@yahoo.com>
2019-09-11 03:37:44 +0200

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-745f6b73bfc021bb45a8.js"
+    "url": "webpack-runtime-5b3f1f5f27ab0a3bfa9b.js"
   },
   {
     "url": "app-9735e24cb3d081716768.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "83a0d92ead50e74c14851739483194c9"
+    "revision": "8351fe175be726c005ba3e6a2030043e"
   },
   {
     "url": "manifest.webmanifest",
