73df3aa841315656fe30a3577a6729dc6f6d9af0
Zeyad Etman <zeyadetman@gmail.com>
2020-04-14 04:06:08 +0200

    update logos import

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,15 +1,16 @@
 import React, { useEffect } from "react";
+import { Document, Page } from "react-pdf";
 
 const Resume = () => {
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      window.location.href = "https://www.slideshare.net/slideshow/embed_code/key/hsbLIjXmKEsnBD";
-    }
-  }, []);
   return (
-    <a href="https://www.slideshare.net/slideshow/embed_code/key/hsbLIjXmKEsnBD">
-      click here if you're not automatically redirected
-    </a>
+    <div>
+      <Document file="somefile.pdf" onLoadSuccess={this.onDocumentLoadSuccess}>
+        <Page pageNumber={pageNumber} />
+      </Document>
+      <p>
+        Page {pageNumber} of {numPages}
+      </p>
+    </div>
   );
 };
 
